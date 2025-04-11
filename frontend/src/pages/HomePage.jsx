c6b35aa0dacd93d0ee351fd29f7d03d5b628ffcb
gaurav <gauravdhall35@gmail.com>
2025-04-11 13:40:21 +0530

    basic setup

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Nav />
+        HomePage</div>
+  )
+}
+
+export default HomePage
