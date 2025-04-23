d0bcb52362dd4d7d1795da0761b43db9af75337b
gaurav <gauravdhall35@gmail.com>
2025-04-24 02:54:15 +0530

    working on cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+
 const dotenv = require('dotenv');
 const port = process.env.PORT || 3000;
 const mongoConnection =require('./config/db')
@@ -11,13 +13,14 @@ const index=require('./routes/index');
 const UserRoute=require('./routes/UserRoute')
 const RecipeRoute=require('./routes/RecipeRoute')
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: ['http://localhost:5173',"https://yum-story.vercel.app/"], // ya jo bhi frontend ka URL ho
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
 app.use(express.static('public'));
